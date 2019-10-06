516c56874c0589f75c07a934ed53b550f16f1b15
Fabricio Rocha <fabriciov.rocha@gmail.com>
2019-10-05 21:11:18 -0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,25 +6,16 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
                     <p>Bem Vindo ao Redes Lógica.</p>
-                    {{-- @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif --}}
-
-                   {{-- @foreach ($users as $user)
-                    <p> <b>Nome:</b> {{$user->name}}</p>
-                    <p> <b>Prontuario:</b> {{$user->prontuario}}</p>
-                    <p> <b>Email:</b> {{$user->email}}</p>
-                   @endforeach --}}
-
-                   {{-- <a href="/campus">Campus</a><a href="/campus/novo">Novo</a> --}}
+                </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+
+
+
+

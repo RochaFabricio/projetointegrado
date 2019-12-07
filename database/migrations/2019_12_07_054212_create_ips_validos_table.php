1986214d7fd2700d3330f1a8900c6a834f22badf
Fabricio Rocha <fabriciov.rocha@gmail.com>
2019-12-07 02:46:21 -0300

    Create 2019_12_07_054212_create_ips_validos_table.php

---
+++ database/migrations/2019_12_07_054212_create_ips_validos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIpsValidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ips_validos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('ip');
+            $table->integer('rede_id');
+            $table->string('uso');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ips_validos');
+    }
+}

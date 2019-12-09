cab094dd8646eaf41681ad5ac498920fda7aa57d
Fabricio Rocha <fabriciov.rocha@gmail.com>
2019-12-08 23:51:21 -0300

    final

--- app/Http/Controllers/SwitchController.php
+++ app/Http/Controllers/SwitchController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Erro;
+use App\Events\Erros;
 use App\Switchs;
 use Illuminate\Http\Request;
 
@@ -16,13 +18,36 @@ class SwitchController extends Controller
 
     public function detalhes(Switchs $switchs, Request $request){
 
+        // dd($switchs);
         return view('switch.detalhes')->with([
             "switchs" => $switchs,
         ]);
     }
 
-    public function createOrNew(Request $request)
+    public function createOrUpdate(Request $request)
     {
-        
+        try {
+            $switchs = Switchs::firstOrNew(['id' => $request->id]);
+            $switchs->marca = $request->marca;
+            $switchs->modelo = $request->modelo;
+            $switchs->qtd_portas = $request->qtd_portas;
+            $switchs->porta_inicio = $request->porta_inicio;
+            $switchs->local_id = $request->local_id;
+            $switchs->rede_ip = $request->rede_ip;
+            $switchs->usuario = $request->usuario;
+            $switchs->senha = $request->senha;
+            $switchs->save();
+        } catch (\Throwable $th) {
+            $erros = new Erro();
+            $erros->view = "Switch.detalhes";
+            $erros->descricao = "Erro ao inserir:".$th;
+
+            event(new Erros($erros));
+
+            return redirect('/switch');
+        }
+        // dd($switchs);
+        return redirect('switch/'.$switchs->id.'/detalhes');
+
     }
 }
--- resources/views/switch/detalhes.blade.php
+++ resources/views/switch/detalhes.blade.php
@@ -5,65 +5,46 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-              @if (!$switchs->exists)
+              @if ($switchs->exists)
               <div class="card-header">Switchs - Novo</div>
               @else
-              <div class="card-header">{{$switchs->nome}} - Editar</div>
+              <div class="card-header">{{$switchs->modelo}} - Editar</div>
               @endif
-
                 <div class="card-body">
-                    <form action="/switchs/novo" method="POST">
+                    <form action="/switch/novo" method="POST">
                       @csrf
                       <input type="hidden" value="{{$switchs->id}}" name="id">
                       <div class="form-group">
                           <label for="sigla">marca</label>
-                          <input type="text" class="form-control" id="marca" name="marca" value="{{$switchs->sigla}}" placeholder="marca">
+                          <input type="text" class="form-control" id="marca" name="marca" value="{{$switchs->marca}}" placeholder="marca">
                         </div>
                       <div class="form-group">
                         <label for="sigla">modelo</label>
-                        <input type="text" class="form-control" id="modelo" name="modelo" value="{{$switchs->sigla}}" placeholder="modelo">
+                        <input type="text" class="form-control" id="modelo" name="modelo" value="{{$switchs->modelo}}" placeholder="modelo">
                       </div>
                       <div class="form-group">
                         <label for="sigla">Quantidade de portas</label>
-                        <input type="text" class="form-control" id="qtd_portas" name="qtd_portas" value="{{$switchs->sigla}}" placeholder="qtd_portas">
+                        <input type="number" class="form-control" id="qtd_portas" name="qtd_portas" value="{{$switchs->sigla}}" placeholder="Quantidade de portas">
                       </div>
                       <div class="form-group">
                         <label for="sigla">Porta de inicio</label>
-                        <input type="text" class="form-control" id="porta_inicio" name="porta_inicio" value="{{$switchs->sigla}}" placeholder="porta_inicio">
+                        <input type="text" class="form-control" id="porta_inicio" name="porta_inicio" value="{{$switchs->sigla}}" placeholder="Porta de inicio">
                       </div>
-                      {{-- <div class="form-group">
-                        <label for="sigla">Local de Uso</label>
-                        <input type="text" class="form-control" id="local_id" name="local_id" value="{{$switchs->sigla}}" placeholder="local_id">
-                      </div> --}}
                       <div class="form-group">
                         <label for="sigla">Local de Uso</label>
-                        <select name="coordenador" id="coordenador" class="form-control">
-                          <option value=""></option>
-                          @foreach ($coordenadores as $coordenador)
-                            <option value="{{$coordenador->id}}">{{$coordenador->name}}</option>
-                          @endforeach
-                        </select>
+                        <input type="text" class="form-control" id="local_id" name="local_id" value="{{$switchs->sigla}}" placeholder="Local de Uso">
                       </div>
                       <div class="form-group">
                         <label for="sigla">Rede</label>
-                        <select name="coordenador" id="coordenador" class="form-control">
-                          <option value=""></option>
-                          @foreach ($coordenadores as $coordenador)
-                            <option value="{{$coordenador->id}}">{{$coordenador->name}}</option>
-                          @endforeach
-                        </select>
+                        <input type="text" class="form-control" id="rede_ip" name="rede_ip" value="{{$switchs->sigla}}" placeholder="Rede">
                       </div>
-                      {{-- <div class="form-group">
-                        <label for="sigla">Rede</label>
-                        <input type="text" class="form-control" id="rede_ip" name="rede_ip" value="{{$switchs->sigla}}" placeholder="rede_ip">
-                      </div> --}}
                       <div class="form-group">
                         <label for="nome">Usuario</label>
-                        <input type="text" class="form-control" id="usuario" name="usuario" placeholder="usuario" value="{{$switchs->nome}}">
+                        <input type="text" class="form-control" id="usuario" name="usuario" placeholder="Usuario" value="{{$switchs->nome}}">
                       </div>
                       <div class="form-group">
                         <label for="nome">Senha</label>
-                        <input type="text" class="form-control" id="usuario" name="usuario" placeholder="usuario" value="{{$switchs->nome}}">
+                        <input type="password" class="form-control" id="senha" name="senha" placeholder="Senha" value="{{$switchs->nome}}">
                       </div>
                         <button type="submit" class="btn btn-primary">Enviar</button>
                       </form>
